d1de38351d49e5c6363186fecee84e24d2be3395
Jiří Hermann <jiri.hermann@gendigital.com>
2024-03-27 13:59:44 +0100

    add kafka articles

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,5 +1,9 @@
+plugins {
+    id("org.gradle.toolchains.foojay-resolver-convention") version "0.5.0"
+}
 rootProject.name = "articles"
 
 include(":article-001")
 include(":article-002")
 include(":article-003")
+include("article-004")
